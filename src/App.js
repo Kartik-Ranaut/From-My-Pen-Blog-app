7af90106180a26f7d5b511c0f92a0721365097d3
Kartik-Ranaut <ranautk6@gmail.com>
2024-01-05 21:51:56 +0530

    layout created basic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div>
+      <div className="header">
+        <div className="wblogo"></div>
+        <div className="Links">
+          <a>Home</a>
+          <a>Blog</a>
+          <a>Latest</a>
+          <a>Contact</a>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="search"></input>
+        </div>
+      </div>
+      <div className="Container"></div>
+    </div>
   );
 }
 
